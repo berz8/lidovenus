96aa2dca68c47a3e568a0668cdede373231b715f
Davide Lombardi <davidelombardi@MacBook-Pro-di-Davide-2.local>
2020-09-04 12:41:47 +0200

    Fix script

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,9 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
-        <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
     </body>
 </html>
